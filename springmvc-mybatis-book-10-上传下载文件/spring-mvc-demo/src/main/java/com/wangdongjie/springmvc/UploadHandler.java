bd5aed6b8905e0635b24baa761d527137b8a9a71
wdj <717273419@qq.com>
2021-04-14 20:41:22 +0800

    增加spring mvc上传下载示例

--- springmvc-mybatis-book-10-上传下载文件/spring-mvc-demo/src/main/java/com/wangdongjie/springmvc/UploadHandler.java
+++ springmvc-mybatis-book-10-上传下载文件/spring-mvc-demo/src/main/java/com/wangdongjie/springmvc/UploadHandler.java
@@ -3,10 +3,18 @@ package com.wangdongjie.springmvc;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
+import java.util.Date;
+
+/*
+
+spring mvc 文件上传的方式。
+
+
+ */
 
 @Controller
 public class UploadHandler {
@@ -17,6 +25,7 @@ public class UploadHandler {
         return "upload";
     }
 
+    //使用MultipartFile方式上传
     @RequestMapping(value = "/uploadfile", method = RequestMethod.POST)
     @ResponseBody
     public String uploadfile(HttpServletRequest request, @RequestParam("information") String information, @RequestParam("file") MultipartFile file) throws IOException {
@@ -38,5 +47,65 @@ public class UploadHandler {
             System.out.println("upload failed");
             return "upload";
         }
+    }
+
+    /*
+     * 通过流的方式上传文件
+     * @RequestParam("file") 将name=file控件得到的文件封装成CommonsMultipartFile 对象
+     * 不推荐此方法，特别慢
+     */
+    @RequestMapping("upload1")
+    public String  fileUpload(@RequestParam("file") CommonsMultipartFile file) throws IOException {
+        //用来检测程序运行时间
+        long  startTime=System.currentTimeMillis();
+        System.out.println("fileName："+file.getOriginalFilename());
+        try {
+            String fileName = "E:\\"+new Date().getTime()+file.getOriginalFilename();
+            File f = new File(fileName);
+            if (!f.exists()){
+                f.getParentFile().mkdir();
+                f.createNewFile();
+            }
+            //获取输出流
+            OutputStream os=new FileOutputStream(fileName);
+            //获取输入流 CommonsMultipartFile 中可以直接得到文件的流
+            InputStream is=file.getInputStream();
+            int temp;
+            //一个一个字节的读取并写入
+            while((temp=is.read())!=(-1))
+            {
+                os.write(temp);
+            }
+            os.flush();
+            os.close();
+            is.close();
+
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        long  endTime=System.currentTimeMillis();
+        System.out.println("方法一的运行时间："+String.valueOf(endTime-startTime)+"ms");
+        return "upload";
+    }
+
+    /*
+     * 采用file.Transto 来保存上传的文件
+     */
+    @RequestMapping("upload2")
+    public String  fileUpload2(@RequestParam("file") CommonsMultipartFile file) throws IOException {
+        long  startTime=System.currentTimeMillis();
+        System.out.println("fileName："+file.getOriginalFilename());
+        String path="E:\\"+new Date().getTime()+file.getOriginalFilename();
+        System.out.println("path："+path);
+        File newFile=new File(path);
+        if (!newFile.exists()) {
+            newFile.getParentFile().mkdirs();
+        }
+        //通过CommonsMultipartFile的方法直接写文件（注意这个时候）
+        file.transferTo(newFile);
+        long  endTime=System.currentTimeMillis();
+        System.out.println("方法二的运行时间："+String.valueOf(endTime-startTime)+"ms");
+        return "upload";
     }
 }
--- springmvc-mybatis-book-10-上传下载文件/spring-mvc-demo/target/spring-mvc-demo/upload.jsp
+++ springmvc-mybatis-book-10-上传下载文件/spring-mvc-demo/target/spring-mvc-demo/upload.jsp
@@ -7,7 +7,7 @@
 <h2>Hello World!</h2>
 <h3>文件上传页面</h3>
 <br>
-<form action="uploadfile" method="post" enctype="multipart/form-data">
+<form name="form" action="uploadfile" method="post" enctype="multipart/form-data">
     <table>
         <tr>
             <td><label>文件信息：</label></td>
@@ -19,6 +19,40 @@
         </tr>
         <tr>
             <td><input type="submit" id="submit" value="上传"></td>
+        </tr>
+    </table>
+</form>
+<h3>文件上传页面2</h3>
+<br>
+<form name="form2" action="upload1" method="post" enctype="multipart/form-data">
+    <table>
+        <tr>
+            <td><label>文件信息：</label></td>
+            <td><input type="text" name="information"></td>
+        </tr>
+        <tr>
+            <td><label>选择文件：</label></td>
+            <td><input type="file" name="file"></td>
+        </tr>
+        <tr>
+            <td><input type="submit" id="submit2" value="上传"></td>
+        </tr>
+    </table>
+</form>
+<h3>文件上传页面</h3>
+<br>
+<form name="form3" action="upload2" method="post" enctype="multipart/form-data">
+    <table>
+        <tr>
+            <td><label>文件信息：</label></td>
+            <td><input type="text" name="information"></td>
+        </tr>
+        <tr>
+            <td><label>选择文件：</label></td>
+            <td><input type="file" name="file"></td>
+        </tr>
+        <tr>
+            <td><input type="submit" id="submit3" value="上传"></td>
         </tr>
     </table>
 </form>
